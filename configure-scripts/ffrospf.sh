bfa21e5313b729e733173c6e41218380a060b63c
Timothy Blamires <tblamires22@gmail.com>
2025-04-13 01:44:16 -0600

    Began work on startup scripts

---
+++ configure-scripts/ffrospf.sh
@@ -0,0 +1,11 @@
+apt -y install curl
+apt -y install gnupg
+curl -s https://deb.frrouting.org/frr/keys.gpg | \
+tee /usr/share/keyrings/frrouting.gpg > /dev/null
+apt install lsb-release
+FRRVER="frr-stable"
+echo deb ’[signed-by=/usr/share/keyrings/frrouting.gpg]’ https://deb.frrouting.org/frr \
+$(lsb_release -s -c) $FRRVER | tee -a /etc/apt/sources.list.d/frr.list
+apt update && apt -y install frr frr-pythontools
+echo "ospfd=yes" >> /etc/frr/daemons
+service frr restart
