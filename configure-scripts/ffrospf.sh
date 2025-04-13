fda47d48c83a37204d41e5e82a558005a75abdd4
Timothy Blamires <tblamires22@gmail.com>
2025-04-13 02:03:05 -0600

    Fix issues with configure scripts

--- configure-scripts/ffrospf.sh
+++ configure-scripts/ffrospf.sh
@@ -4,8 +4,9 @@ curl -s https://deb.frrouting.org/frr/keys.gpg | \
 tee /usr/share/keyrings/frrouting.gpg > /dev/null
 apt install lsb-release
 FRRVER="frr-stable"
-echo deb ’[signed-by=/usr/share/keyrings/frrouting.gpg]’ https://deb.frrouting.org/frr \
+echo deb '[signed-by=/usr/share/keyrings/frrouting.gpg]' https://deb.frrouting.org/frr \
 $(lsb_release -s -c) $FRRVER | tee -a /etc/apt/sources.list.d/frr.list
 apt update && apt -y install frr frr-pythontools
-echo "ospfd=yes" >> /etc/frr/daemons
+mkdir -p /etc/ffr/
+echo "ospfd=yes" > /etc/frr/daemons
 service frr restart
