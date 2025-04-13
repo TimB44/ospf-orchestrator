6161381df1ead63f9602361b8e6e0d8846990aa5
Timothy Blamires <tblamires22@gmail.com>
2025-04-13 14:42:46 -0600

    Clean up orchestrator code

--- orchestrator.py
+++ orchestrator.py
@@ -2,6 +2,7 @@ import argparse
 import subprocess
 import sys
 
+# Constants used in the code
 R1_NAME = "ospf-orchestrator-r1-1"
 R2_NAME = "ospf-orchestrator-r2-1"
 R3_NAME = "ospf-orchestrator-r3-1"
@@ -12,24 +13,24 @@ HB_NAME = "ospf-orchestrator-hb-1"
 ROUTER_NAMES = [R1_NAME, R2_NAME, R3_NAME, R4_NAME]
 HOST_NAMES = [HA_NAME, HB_NAME]
 
+# Starts a docker command and returns the process 
+def run_docker_cmd(container_name: str, cmd: list[str]) -> subprocess.Popen:
+    proc = subprocess.Popen(["docker", "exec", container_name] + cmd, stdin=sys.stdin, stdout=sys.stdout, stderr=sys.stderr, text=True)
+    return proc
 
+# Start the docker network
 def start(_):
     print("Creating docker network")
     proc = subprocess.Popen(["docker", "compose", "up", "-d"], stdin=sys.stdin, stdout=sys.stdout, stderr=sys.stderr, text=True)
     proc.wait()
 
 
-# Starts a docker command and returns the process 
-def run_docker_cmd(container_name: str, cmd: list[str]) -> subprocess.Popen:
-    proc = subprocess.Popen(["docker", "exec", container_name] + cmd, stdin=sys.stdin, stdout=sys.stdout, stderr=sys.stderr, text=True)
-    return proc
-    
+# Installs ffr router on the containers and configures the network
 def configure(_):
     print("Downloading ffr on routers")
     procs = [run_docker_cmd(r, ["./configure-scripts/ffrospf.sh"]) for r in ROUTER_NAMES]
     for proc in  procs:
         proc.wait();
-
 
     print("Configuring ospf on routers")
     procs = [run_docker_cmd(r, [f"./configure-scripts/setup-{r}.sh"]) for r in ROUTER_NAMES]
@@ -42,6 +43,7 @@ def configure(_):
         proc.wait()
 
 
+# Stops the docker network and takes it down
 def stop(_):
     print("Taking down docker network")
     proc = subprocess.Popen(["docker", "compose", "down"], stdin=sys.stdin, stdout=sys.stdout, stderr=sys.stderr, text=True)
@@ -49,17 +51,20 @@ def stop(_):
 
 
 
+# Movies traffic between the northern and southern route
 def route(args):
     dir = args.direction
     if dir == "north":
-        inc_proc = run_docker_cmd(R2_NAME, ["./configure-scripts/increase-cost.sh"])
-        dec_proc = run_docker_cmd(R4_NAME, ["./configure-scripts/decrease-cost.sh"])
+        print("Moving traffic to the northern route")
+        inc_proc = run_docker_cmd(R4_NAME, ["./configure-scripts/increase-cost.sh"])
+        dec_proc = run_docker_cmd(R2_NAME, ["./configure-scripts/decrease-cost.sh"])
         inc_proc.wait()
         dec_proc.wait()
     else:
         assert dir == "south"
-        inc_proc = run_docker_cmd(R4_NAME, ["./configure-scripts/increase-cost.sh"])
-        dec_proc = run_docker_cmd(R2_NAME, ["./configure-scripts/decrease-cost.sh"])
+        print("Moving traffic to the southern route")
+        inc_proc = run_docker_cmd(R2_NAME, ["./configure-scripts/increase-cost.sh"])
+        dec_proc = run_docker_cmd(R4_NAME, ["./configure-scripts/decrease-cost.sh"])
         inc_proc.wait()
         dec_proc.wait()
 
